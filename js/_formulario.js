a561b650bc18f9a06ca43b21d93d8ab315373022
juangrios <juangrios@yahoo.com.ar>
2019-11-06 19:16:24 -0300

    landing hype terminada

---
+++ js/_formulario.js
@@ -0,0 +1,21 @@
+function transform_label1() {
+    document.getElementById('label1').style.height='17px';
+    document.getElementById('label1').style.fontSize="12px";
+    document.getElementById('label1').style.fontWeight="bold";
+    document.getElementById('label1').style.top="9px";
+    document.getElementById('label1').style.lineHeight="1.42";
+}
+function transform_label2() {
+    document.getElementById('label2').style.height='17px';
+    document.getElementById('label2').style.fontSize="12px";
+    document.getElementById('label2').style.fontWeight="bold";
+    document.getElementById('label2').style.top="9px";
+    document.getElementById('label2').style.lineHeight="1.42";
+}
+function transform_label3() {
+    document.getElementById('label3').style.height='17px';
+    document.getElementById('label3').style.fontSize="12px";
+    document.getElementById('label3').style.fontWeight="bold";
+    document.getElementById('label3').style.top="9px";
+    document.getElementById('label3').style.lineHeight="1.42";
+};
